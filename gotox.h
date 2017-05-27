69f2e6be26e7a307240b2e592c208859f548e164
c3rberuss <aaamaya98@gmail.com>
2017-05-26 18:51:33 -0600

    Primeras funciones

---
+++ gotox.h
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+
+Template <class T>
+inline string ToString(const T& t) {
+stringstream ss;
+ss << t;
+return ss.str();
+}
+/* Función para emular al gotoxy en la consola de
+Linux.
+*/
+inline string l_gotoxy(const int& x,const int& y) {
+  return "\33[" + ToString(y) + ";" + ToString(x) + "H"; 
+}
