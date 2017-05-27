a6956495a394ad8eec10d35b13cd96e9f0d63d8e
c3rberuss <aaamaya98@gmail.com>
2017-05-26 18:53:15 -0600

    alineado

--- gotox.h
+++ gotox.h
@@ -4,9 +4,9 @@
 
 Template <class T>
 inline string ToString(const T& t) {
-stringstream ss;
-ss << t;
-return ss.str();
+    stringstream ss;
+    ss << t;
+    return ss.str();
 }
 /* Función para emular al gotoxy en la consola de
 Linux.
